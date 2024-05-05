701f235a782369bbfcbc00c93521cc1ec1cf58d8
dengqi <me@dengqi.org>
2024-05-05 13:40:21 +0800

    code: use another rename method

--- main.go
+++ main.go
@@ -57,33 +57,6 @@ type OpenAIRequest struct {
 	Stream bool `json:"stream"`
 }
 
-type ChatCompletion struct {
-	ID                string   `json:"id"`
-	Object            string   `json:"object"`
-	Created           int64    `json:"created"`
-	Model             string   `json:"model"`
-	SystemFingerprint string   `json:"system_fingerprint"`
-	Choices           []Choice `json:"choices"`
-	Usage             Usage    `json:"usage"`
-}
-
-type Choice struct {
-	Index        int     `json:"index"`
-	Message      Message `json:"message"`
-	FinishReason string  `json:"finish_reason"`
-}
-
-type Message struct {
-	Role    string `json:"role"`
-	Content string `json:"content"`
-}
-
-type Usage struct {
-	PromptTokens     int `json:"prompt_tokens"`
-	CompletionTokens int `json:"completion_tokens"`
-	TotalTokens      int `json:"total_tokens"`
-}
-
 // load file in json and save into table
 func LoadModelTable(filepath string) {
 	// read file
@@ -227,20 +200,25 @@ func ChatCompletions(c *gin.Context) {
 	}
 
 	if resp.StatusCode == http.StatusOK {
-		var chatCompletion ChatCompletion
-		err = json.Unmarshal(respBody, &chatCompletion)
+		var returnJSON map[string]interface{}
+		err = json.Unmarshal(respBody, &returnJSON)
 		if err != nil {
 			log.Println("Failed to unmarshal response body: ", err.Error())
-			log.Println("Response body: ", string(respBody))
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to unmarshal response body"})
 			return
 		}
-		chatCompletion.Model = originModel
-		returnData, err := json.Marshal(chatCompletion)
-		if err != nil {
-			log.Println("Failed to marshal response data: ", err.Error())
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal response data"})
+		for key := range returnJSON {
+			if key == "model" {
+				returnJSON[key] = originModel
+			}
 		}
+
+		returnData, err := json.Marshal(returnJSON)
+		if err != nil {
+			log.Println("Failed to marshal return data: ", err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal return data"})
+		}
+
 		c.Data(resp.StatusCode, resp.Header.Get("Content-Type"), returnData)
 	} else {
 		c.Data(resp.StatusCode, resp.Header.Get("Content-Type"), respBody)
